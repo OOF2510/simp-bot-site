c0ca29694b035b58fc51d5e9828969534647eead
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-07-10 13:16:15 -0400

    update navbar

--- src/elements/Navigation.js
+++ src/elements/Navigation.js
@@ -3,12 +3,22 @@ import { Navbar, Nav } from "react-bootstrap";
 const Navigation = () => {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="/">Simp Bot</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link href="/#">Home</Nav.Link>
-          {/* <Nav.Link href="/#/about">About</Nav.Link> */}
-        </Nav>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="/#">Simp Bot</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/home">Home</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="https://discord.gg/zHtfa8GdPx">
+              Support Server
+            </Nav.Link>
+            <Nav.Link href="https://discord.com/api/oauth2/authorize?client_id=808822189905936405&permissions=8&scope=bot">
+              Invite
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Navbar>
     </>
   );
