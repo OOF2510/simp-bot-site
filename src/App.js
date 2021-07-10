7277464a8310f3a1ddeafee02416746ec673b344
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-07-10 03:03:01 -0400

    Hash routing

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
+import About from "./About";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <nav>
+          <ul id="navigation">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            {/* <li>
+            <link to="/commands">Commands</link>
+          </li>
+          <li>
+            <link to="/invite">Invite</link>
+          </li>
+          <li>
+            <Link to="/support">Support</Link>
+          </li> */}
+          </ul>
+        </nav>
+      </div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </div>
   );
 }
