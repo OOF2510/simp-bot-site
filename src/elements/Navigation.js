5eafa7fab1840189d05c0a8bac8c247a14e1d076
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-07-10 13:36:52 -0400

    update navbar

--- src/elements/Navigation.js
+++ src/elements/Navigation.js
@@ -11,6 +11,7 @@ const Navigation = () => {
             <Nav.Link href="/home">Home</Nav.Link>
           </Nav>
           <Nav>
+            <Nav.Link href="https://git.io/JcpyV" target="_blank">GitHub</Nav.Link>
             <Nav.Link href="https://discord.gg/zHtfa8GdPx" target="_blank">
               Support Server
             </Nav.Link>
