d3f2eb2240746420129f56a219ba5b5e41c0eb91
RobbieCutler <82403176+RobbieCutler@users.noreply.github.com>
2021-08-02 23:06:30 +0100

    Check to see if dropdown menu works

--- src/elements/Navigation.js
+++ src/elements/Navigation.js
@@ -31,6 +31,13 @@ const Navigation = () => {
               Invite
             </Nav.Link>
           </Nav>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
         </Navbar.Collapse>
       </Navbar>
     </>
