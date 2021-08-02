a19b14871b0d50ff7364db55a2647fbd2fe9fcc3
RobbieCutler <82403176+RobbieCutler@users.noreply.github.com>
2021-08-02 23:09:15 +0100

    Navbar check

--- src/elements/Navigation.js
+++ src/elements/Navigation.js
@@ -22,6 +22,9 @@ const Navigation = () => {
             <Nav.Link href="/#/partners" target="_blank">Partners</Nav.Link>
             <Nav.Link href="https://git.io/JcpyV" target="_blank">GitHub</Nav.Link>
             <Nav.Link href="https://discord.gg/zHtfa8GdPx" target="_blank">
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+            </NavDropdown>
               Support Server
             </Nav.Link>
             <Nav.Link
@@ -30,14 +33,6 @@ const Navigation = () => {
             >
               Invite
             </Nav.Link>
-          </Nav>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
         </Navbar.Collapse>
       </Navbar>
     </>
