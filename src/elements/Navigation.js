aa4be44a0db9e97151c2d3ce967f51cba6240b6d
RobbieCutler <82403176+RobbieCutler@users.noreply.github.com>
2021-08-02 23:27:00 +0100

    Attempt to fix

--- src/elements/Navigation.js
+++ src/elements/Navigation.js
@@ -16,16 +16,26 @@ const Navigation = () => {
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
               <Nav className="mr-auto">
-          </Nav>
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/#/partners" target="_blank">Partners</Nav.Link>
-            <Nav.Link href="https://git.io/JcpyV" target="_blank">GitHub</Nav.Link>
-            <Nav.Link href="https://discord.gg/zHtfa8GdPx" target="_blank">
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            </NavDropdown>
+    
+        <Navbar bg="light" expand="lg">
+  <Container>
+    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+    <Navbar.Collapse id="basic-navbar-nav">
+      <Nav className="me-auto">
+        <Nav.Link href="#home">Home</Nav.Link>
+        <Nav.Link href="#link">Link</Nav.Link>
+        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Divider />
+          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+        </NavDropdown>
       </Nav>
+    </Navbar.Collapse>
+  </Container>
+</Navbar>
   );
 };
 
