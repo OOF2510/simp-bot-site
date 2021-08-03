33bf2d907548e968b8360c6312b69380eb87ef9c
RobbieCutler <82403176+RobbieCutler@users.noreply.github.com>
2021-08-03 20:37:23 +0100

    test

--- src/elements/Navigation.jsx
+++ src/elements/Navigation.jsx
@@ -31,6 +31,8 @@ const Navigation = () => {
                 <NavDropdown.Divider />
                 <NavDropdown.Item href="#action/3.4">
                   Separated link
+                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                 </NavDropdown.Item>
               </NavDropdown>
             </Nav>
