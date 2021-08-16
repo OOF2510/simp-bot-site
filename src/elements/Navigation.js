d47dcfce95d82bd227d7dc89ddd7199ebf449a1f
OOF2510 <64423517+OOF2510@users.noreply.github.com>
2021-08-16 15:15:27 -0400

    revert to 6df7bfabeb4e78538a581d4bfa93df4ec30b4e16

---
+++ src/elements/Navigation.js
@@ -0,0 +1,31 @@
+import { Navbar, Nav } from "react-bootstrap";
+
+const Navigation = () => {
+  return (
+    <>
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Navbar.Brand href="/#">Simp Bot</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="https://git.io/JcpyV" target="_blank">GitHub</Nav.Link>
+            <Nav.Link href="https://discord.gg/zHtfa8GdPx" target="_blank">
+              Support Server
+            </Nav.Link>
+            <Nav.Link
+              href="https://discord.com/api/oauth2/authorize?client_id=808822189905936405&permissions=8&scope=bot"
+              target="_blank"
+            >
+              Invite
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </>
+  );
+};
+
+export default Navigation;
